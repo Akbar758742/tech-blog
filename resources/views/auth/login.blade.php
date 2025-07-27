d5db565e505b5f555e4af6eb5fef3880dd47515a
Akbar758742 <mdakbar758742@gmail.com>
2025-07-27 13:46:09 +0600

    Refactor login page layout and enhance sidebar navigation links

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,67 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+<!doctype html>
+<html lang="en">
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('backend') }}/assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="{{ asset('backend') }}/assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('backend') }}/assets/libs/css/style.css">
+    <link rel="stylesheet" href="{{ asset('backend') }}/assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <style>
+        html,
+        body {
+            height: 100%;
+        }
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        body {
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-align: center;
+            align-items: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+        }
+    </style>
+</head>
 
-        <!-- Remember Me -->
+<body>
+    <!-- ============================================================== -->
+    <!-- login page  -->
+    <!-- ============================================================== -->
+    <div class="splash-container">
+        <div class="card ">
+            <div class="card-header text-center"><a href="../index.html"><img class="logo-img"
+                        src="{{ asset('backend') }}/assets/images/logo.png" alt="logo"></a><span
+                    class="splash-description">Please enter your user information.</span></div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <!-- Email Address -->
+                    <div>
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                            :value="old('email')" required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                            autocomplete="current-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                   <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
@@ -39,9 +76,28 @@
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+           
         </div>
-    </form>
-</x-guest-layout>
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+                </form>
+            </div>
+            <div class="card-footer bg-white p-0  ">
+                <div class="card-footer-item card-footer-item-bordered">
+                    <a href="#" class="footer-link">Create An Account</a>
+                </div>
+                <div class="card-footer-item card-footer-item-bordered">
+                    <a href="#" class="footer-link">Forgot Password</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ============================================================== -->
+    <!-- end login page  -->
+    <!-- ============================================================== -->
+    <!-- Optional JavaScript -->
+    <script src="{{ asset('backend') }}/assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
--- resources/views/backend/include/sidebar.blade.php
+++ resources/views/backend/include/sidebar.blade.php
@@ -12,20 +12,20 @@
                                 Menu
                             </li>
                             <li class="nav-item ">
-                                <a class="nav-link {{ \Request::route()->getName()=='dashboard'? 'active':"" }} " href="{{ route('dashboard') }}" ><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
+                                <a class="nav-link {{ \Request::route()->getName()=='dashboard'? 'active':"" }} " href="" ><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
                                
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link {{ \Request::route()->getName()=='category'? 'active':"" }}" href="{{ route('category') }}"data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>category</a>
+                                <a class="nav-link {{ \Request::route()->getName()=='category'? 'active':"" }}" href=""data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>category</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link {{ \Request::route()->getName()=='sub-category'? 'active':"" }}" href="{{ route('sub-category') }}"data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>Sub Category</a>
+                                <a class="nav-link {{ \Request::route()->getName()=='sub-category'? 'active':"" }}" href=""data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>Sub Category</a>
                             </li>
                              <li class="nav-item">
-                                <a class="nav-link {{ \Request::route()->getName()=='product'? 'active':"" }}" href="{{ route('product') }}"data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>Products</a>
+                                <a class="nav-link {{ \Request::route()->getName()=='product'? 'active':"" }}" href=""data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>Products</a>
                             </li>
                              <li class="nav-item">
-                                <a class="nav-link {{ \Request::route()->getName()=='admin.order'? 'active':"" }}" href="{{ route('admin.order') }}"data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>order</a>
+                                <a class="nav-link {{ \Request::route()->getName()=='admin.order'? 'active':"" }}" href=""data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>order</a>
                             </li>
                           
                         </ul>
